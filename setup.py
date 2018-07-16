7ccd8cb3c1d13e5a2e609c435977b9b2b547cc10
Mali Akmanalp <mali@akmanalp.com>
2018-07-16 15:28:50 -0400

    Add pytables to requirements and install_requires

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     ),
     url="http://github.com/cid-harvard/pandas-to-postgres",
     packages=find_packages(),
-    install_requires=["SQLAlchemy", "pandas", "psycopg2"],
+    install_requires=["SQLAlchemy", "pandas", "psycopg2", "tables"],
     long_description=read("README.md"),
     classifiers=[
         "Topic :: Database",
