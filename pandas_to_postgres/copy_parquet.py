8c46767d47e372da53e0052acc2d687d5cc4df7b
ecjackson1821 <ejackson@hks.harvard.edu>
2024-05-09 11:52:36 -0400

    removed begin

--- pandas_to_postgres/copy_parquet.py
+++ pandas_to_postgres/copy_parquet.py
@@ -56,7 +56,7 @@ class ParquetCopy(BaseCopy):
         self.drop_pk()
 
         # These need to be one transaction to use COPY FREEZE
-        with self.conn.begin():
+        with self.conn():
             self.truncate()
             if self.big_copy:
                 self.big_parquet_to_pg(
